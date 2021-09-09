903eabe5082bd7788dd4ae2879a0b8bf541c3fba
DESKTOP-11S94CO\LEE <gentleflo0106@gmail.com>
2021-09-09 13:44:02 +0900

    [이나은]  - 최초 파일 커밋 추가

--- src/main/java/com/gentleflo/memo/MemoApplication.java
+++ src/main/java/com/gentleflo/memo/MemoApplication.java
@@ -1,9 +1,12 @@
 package com.gentleflo.memo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MemoApplication {
 
 	public static void main(String[] args) {
